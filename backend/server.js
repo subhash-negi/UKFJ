dfb36fa8cb72fb807025778c4fb491832fac0d99
Subhash Negi <subhaahnegi@gmail.com>
2024-07-01 21:46:16 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import 'dotenv/config'
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
 const app=express();
-const port=4000
+const port=process.env.PORT || 4000;
 
 //middleware
 app.use(express.json())
